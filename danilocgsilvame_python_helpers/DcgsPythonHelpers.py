53a16bc6f89e9c18798eb8b3aba733a3377fead7
Danilo Silva <contact@danilocgsilva.me>
2020-09-26 17:29:35 -0300

    Datetime object is not required anymore
    The getHashDateFromDate function now have its parameter not required
    anymore. If not provided, uses the current time for timestamp

--- danilocgsilvame_python_helpers/DcgsPythonHelpers.py
+++ danilocgsilvame_python_helpers/DcgsPythonHelpers.py
@@ -1,6 +1,12 @@
+import datetime
+
 class DcgsPythonHelpers:
 
-    def getHashDateFromDate(self, datetime_data):
+    def getHashDateFromDate(self, datetime_data = None):
+
+        if datetime_data == None:
+            datetime_data = datetime.datetime.now()
+
         return str(datetime_data.year) +\
             '{0:02}'.format(datetime_data.month) +\
             '{0:02}'.format(datetime_data.day) + '-' +\
